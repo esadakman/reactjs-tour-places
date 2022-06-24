384ed6b63d0a2d75bfcef8a9e719dbcfd7f7c404
esadakman <esadd26@gmail.com>
2022-06-24 23:33:39 +0300

    cards finish

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -5,14 +5,22 @@ import cardStyle from "../../sass/cards.module.scss";
 
 const Card = () => {
   return (
-    <div className={cardStyle["container"]}>
+    <div className={cardStyle["cardContainer"]}>
       {Data.map((item) => {
         const { id, title, desc, image } = item;
         return (
-          <div className={cardStyle["card"]} key={id}>
-            <h1 className={cardStyle["cityName"]}>{title}</h1>
-            <img className={cardStyle["images"]} src={image} alt="img" />
-            <p className={cardStyle["description"]}>{desc}</p>
+          <div key={id} className={cardStyle["card"]}>
+            <div className={cardStyle["cityName"]}>
+              <h3>{title}</h3>
+            </div>
+
+            <div className={cardStyle["images"]}>
+              <img src={image} alt="images" />
+            </div>
+
+            <div className={cardStyle["desc"]}>
+              <p>{desc}</p>
+            </div>
           </div>
         );
       })}
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,11 @@ import React from "react";
 import headerStyle from "../../sass/header.module.scss";
 
 const Header = () => {
-  return <div className={headerStyle["head"]}>Header</div>;
+  return (
+    <div className={headerStyle["head"]}>
+      <h2>Popular Tour Places</h2>
+    </div>
+  );
 };
 
 export default Header;
